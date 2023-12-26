5bc050785746e94a3f94caad0388c4daa81e2a51
mouleeshwaran <mouleeshwaran619@gmail.com>
2023-12-27 02:03:27 +0530

    Home page changes

--- src/Component.js/Home/Home.js
+++ src/Component.js/Home/Home.js
@@ -4,12 +4,19 @@ import "./Home.css";
 
 const Home = () => {
   var data = product;
+  console.log(data);
   return (
     <div className="home_cointainer">
-      <h1 classname="home_cointainer_header">Home</h1>
-      <div classname="home_items">
+      <h1 className="home_cointainer_header">Home</h1>
+      <div className="home_items">
         {data.map((data) => (
-          <div className="home_item_box">{data.title}</div>
+          <div className="home_item_box">
+            <img class="banner-image" src={data.thumbnail}></img>
+            <h1 className="home_item_name">Name : {data.title} </h1>
+            <div className="home_item_prize">Prize :${data.price}</div>
+            <div className="home_item_rating">Rating :{data.rating}</div>
+            <button class="btn">BUY NOW</button>
+          </div>
         ))}
       </div>
     </div>
